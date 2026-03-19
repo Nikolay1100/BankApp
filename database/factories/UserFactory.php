39b67baf6f6fc4009aa97fc3d15c2c4f5799afec
Nick <nickolai.umrikhin@gmail.com>
2026-03-19 18:04:59 +0100

    Update UserFactory and DatabaseSeeder with sample users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'age' => fake()->numberBetween(18, 80),
+            'balance' => 0,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
@@ -38,8 +40,8 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+        return $this->state(fn(array $attributes) => [
+        'email_verified_at' => null,
         ]);
     }
 }
